dd1e2c0bbd61865339e7dad6ef1c5d29aee8c479
Brendon-He <bjhe@ucsd.edu>
2023-01-26 08:41:44 -0800

    fixed bugs in both Reverse methods, added 2 new testers

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,21 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReversedMultipleNumbers() {
+    int[] input1 = { 3, 5 , 6, 7 };
+    assertArrayEquals(new int[]{7,6,5,3 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+	public void testReverseInPlaceMultipleNumbers() {
+    int[] input1 = { 3, 5 , 6, 7 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 7,6,5,3 }, input1);
+	}
 }
